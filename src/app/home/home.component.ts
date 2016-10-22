b47a6b49b381a980e51dc4f5fe853520ba269fbb
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-22 23:27:32 +0530

    Names of who splits the bill is added here. Add and remove works.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Person } from '../person';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  persons: Person[];
+  name: string;
 
-  constructor() { }
+  constructor() { 
+    this.persons = new Array<Person>();
+    this.name = '';
+  }
 
   ngOnInit() {
+  }
+
+  add(): void {
+    let p = new Person();
+    p.name = this.name;
+    this.persons.push(p);
+    this.name = '';
+  }
+
+  remove(index: number): void {
+    this.persons.splice(index, 1);
   }
 
 }
