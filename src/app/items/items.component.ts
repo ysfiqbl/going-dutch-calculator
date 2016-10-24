fd84b336e344e6f1d5c5a28f25c0e96acf36f5d0
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-24 23:00:15 +0530

    Now adds items to person.

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CalculatorService } from '../calculator.service';
+import { Person } from '../person';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-items',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+  
+  persons: Person[];
+  person: Person;
+  item: Item;
+  items: Item[];
+  personIndex: number;
 
-  constructor() { }
+  constructor(private _cs: CalculatorService, private router: Router) { 
+    this.item = new Item();
+    this.items = new Array<Item>();
+    this.persons = this._cs.get();
+    this.personIndex = 0;
+    this.person = this.persons[this.personIndex];
+  }
 
   ngOnInit() {
+  }
+
+  add() {
+    this.items.push(this.item);
+    this.item = new Item();
+  }
+
+  remove(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  done() {
+    this.persons[this.personIndex].items = this.items;
+    if (this.personIndex != (this.persons.length - 1)) {
+      this.person = this.persons[++this.personIndex];
+      this.items = new Array<Item>();
+      this.item = new Item();
+      console.log(this._cs.get());
+    } else {
+      console.log('All persons done.');
+      console.log(this._cs.get());
+    }
+    
   }
 
 }
