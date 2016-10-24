3279378a9ffc5ffcd3eb3a60ceb5f3388b85958f
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-24 22:58:57 +0530

    Now uses calculator service.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Person } from '../person';
+import { CalculatorService } from '../calculator.service';
 
 @Component({
   selector: 'app-home',
@@ -12,23 +13,24 @@ export class HomeComponent implements OnInit {
   persons: Person[];
   name: string;
 
-  constructor(private router: Router) { 
+  constructor(private _cs: CalculatorService, private router: Router) { 
     this.persons = new Array<Person>();
     this.name = '';
   }
 
   ngOnInit() {
+    this.persons = this._cs.get();
   }
 
   add(): void {
     let p = new Person();
     p.name = this.name;
-    this.persons.push(p);
+    this.persons = this._cs.add(p);
     this.name = '';
   }
 
   remove(index: number): void {
-    this.persons.splice(index, 1);
+    this.persons = this._cs.remove(index);
   }
 
   next() {
