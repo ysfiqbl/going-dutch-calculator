af81e1a4a9dc6516b97280878956f92f567eb6f5
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-24 22:57:39 +0530

    Added get, create and remove person methods.

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Person } from './person';
 
 @Injectable()
 export class CalculatorService {
 
-  constructor() { }
+  persons: Person[];
+
+  constructor() { 
+    this.persons = new Array<Person>();
+  }
+
+  get(): Person[] {
+    return this.persons;
+  }
+
+  add(person): Person[] {
+    this.persons.push(person);
+    return this.persons;
+  }
+
+  remove(index: number): Person[] {
+    this.persons.splice(index, 1);
+    return this.persons;
+  }
 
 }
