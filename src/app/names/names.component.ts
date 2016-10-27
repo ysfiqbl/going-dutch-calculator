8a3d27f5868be2eeacd639e302d7a43725b04504
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-27 22:03:05 +0530

    Now handles names.

--- src/app/names/names.component.ts
+++ src/app/names/names.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Person } from '../person';
+import { CalculatorService } from '../calculator.service';
+
 
 @Component({
   selector: 'app-names',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NamesComponent implements OnInit {
 
-  constructor() { }
+  persons: Person[];
+  name: string;
+
+  constructor(private _cs: CalculatorService, private router: Router) { 
+    this.persons = new Array<Person>();
+    this.name = '';
+  }
 
   ngOnInit() {
+    this.persons = this._cs.get();
   }
+
+  add(): void {
+    let p = new Person();
+    p.name = this.name;
+    this.persons = this._cs.add(p);
+    this.name = '';
+  }
+
+  remove(index: number): void {
+    this.persons = this._cs.remove(index);
+  }
+
+  next() {
+    this.router.navigateByUrl('/items');
+  }
+
 
 }
