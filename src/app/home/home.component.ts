91f96a73fd025ccb96049404d2526b516086c18b
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-27 22:01:40 +0530

    Now captures grandtotal and subtotal.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,31 +10,21 @@ import { CalculatorService } from '../calculator.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  persons: Person[];
-  name: string;
+  subtotal: number;
+  grandtotal: number
 
   constructor(private _cs: CalculatorService, private router: Router) { 
-    this.persons = new Array<Person>();
-    this.name = '';
+    
   }
 
   ngOnInit() {
-    this.persons = this._cs.get();
-  }
-
-  add(): void {
-    let p = new Person();
-    p.name = this.name;
-    this.persons = this._cs.add(p);
-    this.name = '';
-  }
-
-  remove(index: number): void {
-    this.persons = this._cs.remove(index);
+    
   }
 
   next() {
-    this.router.navigateByUrl('/items');
+    this._cs.setSubTotal(this.subtotal);
+    this._cs.setGrandTotal(this.grandtotal);
+    this.router.navigateByUrl('/names');
   }
 
 }
