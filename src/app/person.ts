ba3cee205c26ed1e1b66f01c5bc160ff32071354
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-31 18:20:03 +0530

    Added method to calculate subtotal.

--- src/app/person.ts
+++ src/app/person.ts
@@ -3,4 +3,18 @@ import { Item } from './item';
 export class Person {
     name: string;
     items: Item[];
+
+    constructor() {
+        this.name = '';
+        this.items = new Array<Item>();
+    }
+
+    subtotal() :number {
+        let subtotal = 0; 
+        for (var i = 0; i < this.items.length; i++) {
+            subtotal += this.items[i].amount;
+        }
+
+        return subtotal;
+    }
 }
